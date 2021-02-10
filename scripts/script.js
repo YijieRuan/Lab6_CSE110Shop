20bcd6b209da9a0f4fba943bfe9b705651616448
Yijie Ruan <yiruan@ucsd.edu>
2021-02-11 01:58:11 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,32 +4,8 @@ window.addEventListener('DOMContentLoaded', () => {
   var myLocalStorage = window.localStorage;
   var cart = [];
   myLocalStorage.setItem('cart', JSON.stringify(cart));
-  if(!myLocalStorage.getItem('products')){
-    fetch('https://fakestoreapi.com/products')
-      .then(response => response.json())
-      .then(data =>{
-        myLocalStorage.setItem('products', JSON.stringify(data));
-        var storage = JSON.parse(myLocalStorage.getItem('products'));
-
-        
-        storage.forEach(element => {
-          //product-item initialize
-          var pItem = document.createElement('product-item');
-          pItem.setAttribute('img', element.image);
-          pItem.setAttribute('alt', element.title);
-          pItem.setAttribute('price','$' + element.price);
-          pItem.setAttribute('dataId', element.id);
-          // pItem.setAttribute('description', element.description);
-          pItem.setAttribute('title', element.title);
-          // pItem.setAttribute('category', element.category);
-          document.getElementById('product-list').appendChild(pItem);
-        });
-      });
-  }
-  else{
-    const storage = JSON.parse(myLocalStorage.getItem('products'));
-
-    
+  if(myLocalStorage.getItem('products')){
+    var storage = JSON.parse(myLocalStorage.getItem('products'));
     storage.forEach(element => {
       //product-item initialize
       var pItem = document.createElement('product-item');
@@ -43,6 +19,26 @@ window.addEventListener('DOMContentLoaded', () => {
       // pItem.setAttribute('category', element.category);
       document.getElementById('product-list').appendChild(pItem);
     });
+  }
+  else{
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data =>{
+        myLocalStorage.setItem('products', JSON.stringify(data));
+        var storage = JSON.parse(myLocalStorage.getItem('products'));
+        storage.forEach(element => {
+          //product-item initialize
+          var pItem = document.createElement('product-item');
+          pItem.setAttribute('img', element.image);
+          pItem.setAttribute('alt', element.title);
+          pItem.setAttribute('price','$' + element.price);
+          pItem.setAttribute('dataId', element.id);
+          // pItem.setAttribute('description', element.description);
+          pItem.setAttribute('title', element.title);
+          // pItem.setAttribute('category', element.category);
+          document.getElementById('product-list').appendChild(pItem);
+        });
+      });
   }
   
 });
